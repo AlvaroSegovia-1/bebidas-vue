e95a7606cdccabd0fd42cc109afd11f7bb666680
Alvagit25 <alvarodesegovia@gmail.com>
2023-12-13 10:32:38 +0100

    iniiciar proyecto

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+
+</script>
+
+<template>  
+  <header>
+    <div class="mx-auto container px-5 py-16">
+
+        <div>
+         Hola
+        </div>
+
+        <form 
+           class=" md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6 ">
+             <div class="space-y-4">
+               <label
+                 class="block text-white uppercase font-extrabold text-lg"
+                 for="">
+               Nombre o Ingredientes
+               </label>
+
+             </div>        
+        </form>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
